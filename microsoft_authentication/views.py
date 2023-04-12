2c0720f601a2de927e98f93df4d8d14de96b9071
Dr. Shubham Dipt <shubham.dipt@gmail.com>
2023-04-13 00:39:45 +0200

    [feature] Support for redirect next url

--- microsoft_authentication/views.py
+++ microsoft_authentication/views.py
@@ -1,22 +1,24 @@
-from django.http import HttpResponseRedirect, HttpResponseForbidden
-from django.contrib.auth import login, logout
 from django.conf import settings
+from django.contrib.auth import login, logout
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+
 from microsoft_authentication.auth.auth_utils import (
+    get_django_user,
+    get_logout_url,
     get_sign_in_flow,
     get_token_from_code,
     get_user,
-    get_django_user,
-    get_logout_url,
 )
 
 
 def microsoft_login(request):
+    request.session["next_url"] = request.GET.get("next")
     flow = get_sign_in_flow()
     try:
-        request.session['auth_flow'] = flow
+        request.session["auth_flow"] = flow
     except Exception as e:
         print(e)
-    return HttpResponseRedirect(flow['auth_uri'])
+    return HttpResponseRedirect(flow["auth_uri"])
 
 
 def microsoft_logout(request):
@@ -26,10 +28,13 @@ def microsoft_logout(request):
 
 def callback(request):
     result = get_token_from_code(request)
-    ms_user = get_user(result['access_token'])
-    user = get_django_user(email=ms_user['mail'])
+    next_url = request.session.pop("next_url", None)
+    ms_user = get_user(result["access_token"])
+    user = get_django_user(email=ms_user["mail"])
     if user:
         login(request, user)
     else:
         return HttpResponseForbidden("Invalid email for this app.")
+    if next_url:
+        return HttpResponseRedirect(next_url)
     return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL or "/admin")
