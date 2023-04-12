4a5c9bbd1d9e2c004ad70fe51a37df324bd71b0b
Dr. Shubham Dipt <shubham.dipt@gmail.com>
2023-04-13 00:50:33 +0200

    [fix] When mail is Null, use userPrincipalName

--- microsoft_authentication/views.py
+++ microsoft_authentication/views.py
@@ -30,7 +30,7 @@ def callback(request):
     result = get_token_from_code(request)
     next_url = request.session.pop("next_url", None)
     ms_user = get_user(result["access_token"])
-    user = get_django_user(email=ms_user["mail"])
+    user = get_django_user(email=ms_user.get("mail") or ms_user.get("userPrincipalName"))
     if user:
         login(request, user, backend="django.contrib.auth.backends.ModelBackend")
     else:
